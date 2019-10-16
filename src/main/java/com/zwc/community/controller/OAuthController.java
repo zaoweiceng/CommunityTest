4063a15ada64fccafebb7c34557499ccdb98b9a5
zaoweiceng <zaoweiceng@qq.com>
2019-10-16 18:20:17 +0800

    log in change2

--- src/main/java/com/zwc/community/controller/OAuthController.java
+++ src/main/java/com/zwc/community/controller/OAuthController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class OAuthController {
 
@@ -26,7 +28,8 @@ public class OAuthController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam (name = "code" ) String code,
-                           @RequestParam (name = "state") String state){
+                           @RequestParam (name = "state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(redirectUri);
@@ -36,6 +39,11 @@ public class OAuthController {
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GitHubUser user = githubProvider.getUser(accessToken);
         System.out.println(user.getName());
-        return "index";
+        if (user != null){
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else {
+            return "redirect:/";
+        }
     }
 }
